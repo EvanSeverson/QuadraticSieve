ebf0d3675e4dbc4da92b940f08bb5ac2156ae88d
Evan Severson <severson.evan@gmail.com>
2020-12-18 18:27:14 -0800

    Implement fermat method

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::io;
+use std::str::FromStr;
+use gmp::mpz::Mpz;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please enter the number you wish to factor");
+
+    let mut val = String::new();
+    io::stdin()
+        .read_line(&mut val)
+        .expect("Failed to read line");
+    let val = match gmp::mpz::Mpz::from_str(&val) {
+        Ok(s) => {s}
+        Err(_) => {
+            println!("Bad number.");
+            return;
+        }
+    };
+    let factor = findNontrivialFactor(&val);
+
+    println!("a factor is {}", factor);
+}
+
+fn findNontrivialFactor(val: &Mpz) -> Mpz {
+    let sqrt = val.sqrt();
+    let mut a = sqrt + 1;
+    while &a < &val {
+        let b2 = (&a * &a) % val;
+        let b = b2.sqrt();
+
+        if &b * &b == b2 {
+            let x = val.gcd(&(&a - &b));
+            if x != Mpz::one() {
+                return x;
+            }
+        }
+        a = &a + 1;
+    }
+
+    return Mpz::one();
 }
