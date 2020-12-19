57b650cebd0fe4be0cdaf85353166b0fc93d3396
Evan Severson <severson.evan@gmail.com>
2020-12-19 11:43:39 -0800

    Implement fermat using the standard bigints

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::io;
+use std::{io, mem};
 use std::str::FromStr;
-use gmp::mpz::Mpz;
+use num_bigint::{BigUint, ParseBigIntError};
+use num_traits::One;
+use std::mem::swap;
 
 fn main() {
     println!("Please enter the number you wish to factor");
@@ -9,33 +11,62 @@ fn main() {
     io::stdin()
         .read_line(&mut val)
         .expect("Failed to read line");
-    let val = match gmp::mpz::Mpz::from_str(&val) {
+    let val: BigUint = match BigUint::from_str(&val.trim()) {
         Ok(s) => {s}
         Err(_) => {
             println!("Bad number.");
             return;
         }
     };
+
     let factor = findNontrivialFactor(&val);
 
     println!("a factor is {}", factor);
 }
 
-fn findNontrivialFactor(val: &Mpz) -> Mpz {
+fn findNontrivialFactor(val: &BigUint) -> BigUint {
     let sqrt = val.sqrt();
-    let mut a = sqrt + 1;
+    let mut a: BigUint = sqrt + BigUint::one();
     while &a < &val {
         let b2 = (&a * &a) % val;
         let b = b2.sqrt();
 
         if &b * &b == b2 {
-            let x = val.gcd(&(&a - &b));
-            if x != Mpz::one() {
+            // let x = val.gcd(&(&a - &b));
+            let x = gcd(val, &(&a - &b));
+            if x != BigUint::one() {
                 return x;
             }
         }
-        a = &a + 1;
+        a = &a + BigUint::one();
     }
 
-    return Mpz::one();
+    return BigUint::one();
+}
+
+fn gcd(a: &BigUint, b: &BigUint) -> BigUint {
+    let zero: u8 = 0;
+    let zero = &BigUint::from(zero);
+    if a == zero {
+        return b.clone();
+    }
+    if b == zero {
+        return a.clone();
+    }
+
+    let mut m = a.clone();
+    let mut n = b.clone();
+
+    if m > n {
+        swap(&mut m, &mut n);
+    }
+
+
+    while &m != zero {
+        let tmp = &n % &m;
+        n = m;
+        m = tmp;
+    }
+
+    return n;
 }
