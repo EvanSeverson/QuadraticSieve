708bcec5b4232402363fe84603e182cf2983aa7d
Evan Severson <severson.evan@gmail.com>
2020-12-19 11:59:36 -0800

    Time using BigUint

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,53 @@ use std::str::FromStr;
 use num_bigint::{BigUint, ParseBigIntError};
 use num_traits::One;
 use std::mem::swap;
+use std::time::SystemTime;
 
 fn main() {
-    println!("Please enter the number you wish to factor");
+    // println!("Please enter the number you wish to factor");
 
-    let mut val = String::new();
-    io::stdin()
-        .read_line(&mut val)
-        .expect("Failed to read line");
-    let val: BigUint = match BigUint::from_str(&val.trim()) {
-        Ok(s) => {s}
-        Err(_) => {
-            println!("Bad number.");
-            return;
+    // let mut val = String::new();
+    // io::stdin()
+    //     .read_line(&mut val)
+    //     .expect("Failed to read line");
+    // let val: BigUint = match BigUint::from_str(&val.trim()) {
+    //     Ok(s) => {s}
+    //     Err(_) => {
+    //         println!("Bad number.");
+    //         return;
+    //     }
+    // };
+
+    let vals = ["238305947612245937", "119886712242830033", "347904798577057303",
+        "45211667757781417", "223302725014881931", "72216575918196841",
+        "522112699961772923", "276590696103561107", "801139792682721823",
+        "307443312955238791"];
+
+    let now = SystemTime::now();
+
+    for val in &vals {
+        let val: BigUint = match BigUint::from_str(&val.trim()) {
+            Ok(s) => {s}
+            Err(_) => {
+                println!("Bad number.");
+                return;
+            }
+        };
+        let factor = findNontrivialFactor(&val);
+
+        println!("a factor for {} is {}", val, factor);
+    }
+
+
+
+
+    match now.elapsed() {
+        Ok(elapsed) => {
+            println!("took {} micros", elapsed.as_micros());}
+        Err(e) => {
+            println!("Error: {:?}", e);
         }
-    };
-
-    let factor = findNontrivialFactor(&val);
-
-    println!("a factor is {}", factor);
+    }
 }
 
 fn findNontrivialFactor(val: &BigUint) -> BigUint {
