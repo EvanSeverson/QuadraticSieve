977aca3adb97988ba89f55af1e2fb7836a7b7f33
Evan Severson <severson.evan@gmail.com>
2020-12-19 12:18:49 -0800

    Benchmark gmp

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,22 @@
-use std::{io, mem};
+use std::io;
 use std::str::FromStr;
-use num_bigint::{BigUint, ParseBigIntError};
-use num_traits::One;
-use std::mem::swap;
+use gmp::mpz::Mpz;
 use std::time::SystemTime;
 
 fn main() {
-    // println!("Please enter the number you wish to factor");
 
     // let mut val = String::new();
     // io::stdin()
     //     .read_line(&mut val)
     //     .expect("Failed to read line");
-    // let val: BigUint = match BigUint::from_str(&val.trim()) {
+    // let val = match gmp::mpz::Mpz::from_str(&val) {
     //     Ok(s) => {s}
     //     Err(_) => {
     //         println!("Bad number.");
     //         return;
     //     }
     // };
+
 
     let vals = ["238305947612245937", "119886712242830033", "347904798577057303",
         "45211667757781417", "223302725014881931", "72216575918196841",
@@ -28,7 +26,7 @@ fn main() {
     let now = SystemTime::now();
 
     for val in &vals {
-        let val: BigUint = match BigUint::from_str(&val.trim()) {
+        let val = match gmp::mpz::Mpz::from_str(&val) {
             Ok(s) => {s}
             Err(_) => {
                 println!("Bad number.");
@@ -40,9 +38,6 @@ fn main() {
         println!("a factor for {} is {}", val, factor);
     }
 
-
-
-
     match now.elapsed() {
         Ok(elapsed) => {
             println!("took {} micros", elapsed.as_micros());}
@@ -52,49 +47,21 @@ fn main() {
     }
 }
 
-fn findNontrivialFactor(val: &BigUint) -> BigUint {
+fn findNontrivialFactor(val: &Mpz) -> Mpz {
     let sqrt = val.sqrt();
-    let mut a: BigUint = sqrt + BigUint::one();
+    let mut a = sqrt + 1;
     while &a < &val {
         let b2 = (&a * &a) % val;
         let b = b2.sqrt();
 
         if &b * &b == b2 {
-            // let x = val.gcd(&(&a - &b));
-            let x = gcd(val, &(&a - &b));
-            if x != BigUint::one() {
+            let x = val.gcd(&(&a - &b));
+            if x != Mpz::one() {
                 return x;
             }
         }
-        a = &a + BigUint::one();
+        a = &a + 1;
     }
 
-    return BigUint::one();
-}
-
-fn gcd(a: &BigUint, b: &BigUint) -> BigUint {
-    let zero: u8 = 0;
-    let zero = &BigUint::from(zero);
-    if a == zero {
-        return b.clone();
-    }
-    if b == zero {
-        return a.clone();
-    }
-
-    let mut m = a.clone();
-    let mut n = b.clone();
-
-    if m > n {
-        swap(&mut m, &mut n);
-    }
-
-
-    while &m != zero {
-        let tmp = &n % &m;
-        n = m;
-        m = tmp;
-    }
-
-    return n;
+    return Mpz::one();
 }
