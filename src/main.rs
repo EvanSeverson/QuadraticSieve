5e643ae5e79fb35c76167d27e0e5eabddeca4252
Evan Severson <severson.evan@gmail.com>
2020-12-20 15:17:31 -0800

    Tmp, tried using a u512 crate.
    I will try to implement u512 for myself

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,41 @@
 use std::io;
 use std::str::FromStr;
 use gmp::mpz::Mpz;
+use num_integer::{Roots, Integer};
+use std::time::SystemTime;
+use primitive_types::U512;
+use num_traits::{Num, One, Zero};
 
 fn main() {
     println!("Please enter the number you wish to factor");
 
-    let mut val = String::new();
-    io::stdin()
-        .read_line(&mut val)
-        .expect("Failed to read line");
-    let val = match gmp::mpz::Mpz::from_str(&val) {
-        Ok(s) => {s}
-        Err(_) => {
-            println!("Bad number.");
-            return;
-        }
-    };
-    let factor = findNontrivialFactor(&val);
+    // let mut val = String::new();
+    // io::stdin()
+    //     .read_line(&mut val)
+    //     .expect("Failed to read line");
+    // let val = &val.trim().parse().expect("Failed to parse");
 
-    println!("a factor is {}", factor);
+    let vals = ["238305947612245937", "119886712242830033", "347904798577057303",
+        "45211667757781417", "223302725014881931", "72216575918196841",
+        "522112699961772923", "276590696103561107", "801139792682721823",
+        "307443312955238791"];
+    let now = SystemTime::now();
+    for val in &vals {
+        let val = U512::from_dec_str(&val.trim()).expect("Unable to parse");
+        let factor = findNontrivialFactor(&val);
+
+        println!("a factor for {} is {}", val, factor);
+    }
+    match now.elapsed() {
+        Ok(elapsed) => {
+            println!("took {} micros", elapsed.as_micros());}
+        Err(e) => {
+            println!("Error: {:?}", e);
+        }
+    }
 }
 
-fn findNontrivialFactor(val: &Mpz) -> Mpz {
+fn findNontrivialFactor(val: &U512) -> U512 {
     let sqrt = val.sqrt();
     let mut a = sqrt + 1;
     while &a < &val {
@@ -29,13 +43,30 @@ fn findNontrivialFactor(val: &Mpz) -> Mpz {
         let b = b2.sqrt();
 
         if &b * &b == b2 {
-            let x = val.gcd(&(&a - &b));
-            if x != Mpz::one() {
+            let x = gcd(&val, &(&a - &b));
+            if x != 1 {
                 return x;
             }
         }
         a = &a + 1;
     }
 
-    return Mpz::one();
+    return U512::one();
+}
+
+fn gcd(a: &U512, b: &U512) -> U512 {
+    let mut a = *a;
+    let mut b = *b;
+
+    if a < b {
+        std::mem::swap(&mut a, &mut b);
+    }
+
+    while b != 0 {
+        let tmp = a % b;
+        a = b;
+        b = tmp;
+    }
+
+    return a;
 }
