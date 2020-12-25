b1da6156d22f4847817400abcb0150c40d06b76d
Evan Severson <severson.evan@gmail.com>
2020-12-25 14:36:11 -0800

    Change multiplcation impl.

--- src/U512.rs
+++ src/U512.rs
@@ -5,6 +5,7 @@ use std::cmp::Ordering;
 use std::fmt::{Display, Formatter, Error};
 
 #[derive(Debug)]
+#[derive(Copy, Clone)]
 pub struct U512 {
     x7: u64,
     x6: u64,
@@ -19,6 +20,25 @@ pub struct U512 {
 impl U512 {
     pub fn asArray(&self) -> [u64; 8] {
         return [self.x7, self.x6, self.x5, self.x4, self.x3, self.x2, self.x1, self.x0, ];
+    }
+
+    pub fn mulSlow(self, rhs: U512) -> U512 {
+        let x = self.asArray();
+        let y = rhs.asArray();
+
+        let mut z: [u64; 8] = [0; 8];
+
+        let mut carry_val: u128 = 0;
+        for i in (0..8).rev() {
+            for j in ((7 - i)..8).rev() {
+                let k = (i + j - 7) as usize;
+                carry_val = (x[i] as u128) * (y[j] as u128) + (z[k] as u128) + carry_val;
+
+                z[k] = carry_val as u64;
+                carry_val = carry_val >> 64;
+            }
+        }
+        return fromArray(z);
     }
 }
 
@@ -149,17 +169,46 @@ impl Mul<U512> for U512 {
         let x = self.asArray();
         let y = rhs.asArray();
 
-        let mut z: [u64; 8] = [0; 8];
+        let mut z1: [u64; 4] = [0; 4];
+        let mut z2: [u64; 4] = [0; 4];
+        let mut carry_val1: u128 = 0;
+        let mut carry_val2: u128 = 0;
+        for i in (0..4).rev() {
+            for j in ((3 - i)..4).rev() {
+                let k = (i + j - 3) as usize;
 
+                carry_val1 = (x[i] as u128) * (y[j] as u128) + (z1[k] as u128) + carry_val1;
+                z1[k] = carry_val1 as u64;
+                carry_val1 = carry_val1 >> 64;
+
+                carry_val2 = (x[i] as u128 - x[i + 4] as u128) * (y[j] as u128 - y[j + 4] as u128) + (z2[k] as u128) + carry_val2;
+                z2[k] = carry_val2 as u64;
+                carry_val2 = carry_val2 >> 64;
+
+            }
+        }
+
+        let mut z3: [u64; 8] = [0; 8];
         let mut carry_val: u128 = 0;
-        for i in (0..8).rev() {
-            for j in ((7 - i)..8).rev() {
-                let k = (i + j - 7) as usize;
-                carry_val = (x[i] as u128) * (y[j] as u128) + (z[k] as u128) + carry_val;
-
-                z[k] = carry_val as u64;
+        for i in (0..4).rev() {
+            for j in (0..4).rev() {
+                let k = (i + j + 1) as usize;
+                carry_val = (x[i + 4] as u128) * (y[j + 4] as u128) + (z3[k] as u128) + carry_val;
+                z3[k] = carry_val as u64;
                 carry_val = carry_val >> 64;
             }
+        }
+        z3[0] = carry_val as u64;
+
+        let mut z: [u64; 8] = [0; 8];
+        let mut carry_val = 0;
+        for i in (4..8).rev() {
+            z[i] = z3[i];
+        }
+        for i in (0..4).rev() {
+            carry_val = (z1[i] as u128) + (z3[i + 4] as u128) + (z3[i] as u128) + carry_val - (z2[i] as u128);
+            z[i] = carry_val as u64;
+            carry_val = carry_val >> 64;
         }
         return fromArray(z);
     }
