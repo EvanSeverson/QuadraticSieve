4f3f7647ddea27a5dac8265b4638a7ddd74aa91f
Evan Severson <severson.evan@gmail.com>
2020-12-27 09:12:23 -0800

    Added tests for U512

--- src/U512.rs
+++ src/U512.rs
@@ -191,6 +191,7 @@ impl Mul<U512> for U512 {
                 z[k] = carry_val as u64;
                 carry_val = carry_val >> 64;
             }
+            carry_val = 0;
         }
         return fromArray(z);
     }
@@ -240,5 +241,134 @@ impl Display for U512 {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result<(), Error> {
         write!(f, "{},{},{},{},{},{},{},{}",
                self.x7, self.x6, self.x5, self.x4, self.x3, self.x2, self.x1, self.x0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::U512::{U512, from};
+
+    #[test]
+    fn testAdditionU128() {
+        let mut a: u128 = 2;
+        let mut b: u128 = 3;
+        for _ in 1..1000000 {
+            let a2 = U512::from(a);
+            let b2 = U512::from(b);
+
+            let c = a + b;
+            let c2 = a2 + b2;
+
+            assert_eq!(c as u64, c2.x0);
+            assert_eq!((c >> 64) as u64, c2.x1);
+
+            a = a.wrapping_mul(a).wrapping_add(1) % (1u128 << 63);
+            b = b.wrapping_mul(b).wrapping_add(1) % (1u128 << 63);
+        }
+    }
+
+    #[test]
+    fn testSubtractionU128() {
+        let mut a: u128 = 2;
+        let mut b: u128 = 3;
+        for _ in 1..1000000 {
+            if b > a {
+                let tmp = a;
+                a = b;
+                b = tmp;
+            }
+            let a2 = U512::from(a);
+            let b2 = U512::from(b);
+
+            let c = a - b;
+            let c2 = a2 - b2;
+
+            assert_eq!(c as u64, c2.x0);
+            assert_eq!((c >> 64) as u64, c2.x1);
+
+            a = a.wrapping_mul(a).wrapping_add(1) % (1u128 << 63);
+            b = b.wrapping_mul(b).wrapping_add(1) % (1u128 << 63);
+        }
+    }
+
+    #[test]
+    fn testAdditionSubtractionInverse() {
+        let mut a = U512::from(2);
+        let mut b = U512::from(3);
+
+        for _ in 1..1000000 {
+            let c = a + b;
+            let a2 = c - b;
+            let b2 = c - a;
+
+            assert_eq!(a, a2);
+            assert_eq!(b, b2);
+
+            a = a * a + U512::from(1);
+            b = b * b + U512::from(1);
+        }
+    }
+
+    #[test]
+    fn testAdditionCommutative() {
+        let mut a = U512::from(2);
+        let mut b = U512::from(3);
+
+        for _ in 1..1000000 {
+
+            assert_eq!(a + b, b + a);
+
+            a = a * a + U512::from(1);
+            b = b * b + U512::from(1);
+        }
+    }
+
+    // This test actually found a bug that existed since the beginning. Binary doubling, analogous
+    // to the binary squaring algorithm for exponentiation, isn't a very optimal algorithm but if
+    // the test is run with --release then it runs about 60x faster so you can increase the number
+    // of test cases.
+    #[test]
+    fn testMultiplicationVsBinaryAddition() {
+        let mut a = U512::from(2);
+        let mut b = U512::from(3);
+
+        for _ in 1..20000 {
+            let c = a * b;
+
+            let mut c2 = U512::from(0);
+            let mut a2 = a;
+            let mut b2 = b;
+            while b2 > U512::from(0) {
+                if b2.x0 % 2 == 1 {
+                    c2 = c2 + a2;
+                }
+                // a2 = a2 + a2;
+                a2 = U512::fromArray([
+                    (a2.x7 << 1) + (a2.x6 >> 63),
+                    (a2.x6 << 1) + (a2.x5 >> 63),
+                    (a2.x5 << 1) + (a2.x4 >> 63),
+                    (a2.x4 << 1) + (a2.x3 >> 63),
+                    (a2.x3 << 1) + (a2.x2 >> 63),
+                    (a2.x2 << 1) + (a2.x1 >> 63),
+                    (a2.x1 << 1) + (a2.x0 >> 63),
+                    (a2.x0 << 1),
+                ]);
+                b2 = U512::fromArray([
+                    (b2.x7 >> 1),
+                    (b2.x6 >> 1) + ((b2.x7 % 2) << 63),
+                    (b2.x5 >> 1) + ((b2.x6 % 2) << 63),
+                    (b2.x4 >> 1) + ((b2.x5 % 2) << 63),
+                    (b2.x3 >> 1) + ((b2.x4 % 2) << 63),
+                    (b2.x2 >> 1) + ((b2.x3 % 2) << 63),
+                    (b2.x1 >> 1) + ((b2.x2 % 2) << 63),
+                    (b2.x0 >> 1) + ((b2.x1 % 2) << 63),
+                ]);
+            }
+
+            assert_eq!(c, c2, "Testing multiplying {} and {}", a, b);
+
+            a = a * a + U512::from(1);
+            b = b * b + U512::from(1);
+        }
     }
 }
